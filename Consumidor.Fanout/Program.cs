fe92ec63ab680bf7d0fa272a8cf22f33b28a9a83
Marcos Sabino <marcos.sabino@outlook.com>
2020-07-20 22:30:47 -0300

    Fila Fanout

--- Consumidor.Fanout/Program.cs
+++ Consumidor.Fanout/Program.cs
@@ -3,16 +3,26 @@ using RabbitMQ.Client.Events;
 using System;
 using System.Text;
 
-namespace Consumidor
+namespace Consumidor.Fanout
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+
+			/*
+			 * ANTES DE CHAMAR ESSE EXE
+			 * CHAMAR O EXE Produtor.Task
+			 * ELE QUEM GERA OS ITENS NO EXCHANGE
+			 * 
+			 */
+			Console.Write("Digite o nome da fila: ");
+			string queueName = Console.ReadLine();
+
             var factory = new ConnectionFactory() { HostName = "localhost" };
 			using var connection = factory.CreateConnection();
 			using var channel = connection.CreateModel();
-			channel.QueueDeclare(queue: "pedidoRoundRobin",
+			channel.QueueDeclare(queue: queueName,
 								 durable: false,
 								 exclusive: false,
 								 autoDelete: false,
@@ -25,6 +35,8 @@ namespace Consumidor
 			 */
 			channel.BasicQos(0, 1, false);
 
+			Console.WriteLine(" [*] Waiting for logs.");
+
 			var consumer = new EventingBasicConsumer(channel);
 			consumer.Received += (model, ea) =>
 			{
@@ -32,22 +44,16 @@ namespace Consumidor
 				{
 					var body = ea.Body.ToArray();
 					var message = Encoding.UTF8.GetString(body);
-					Console.WriteLine(" [x] Received {0}", message);
-
-					/*
-					 * https://www.rabbitmq.com/confirms.html
-					 * PARA REMOVAR DA LISTA, APENAS QUANDO A REGRA DE NEGÓCIO ESTIVER OK
-					 */
 					channel.BasicAck(ea.DeliveryTag, false);
+					Console.WriteLine(" [x] {0}", message);
 				}
 				catch (Exception)
 				{
-					// VOLTA PRA LISTA SE HOUVER UM ERRO DURANTE O PROCESSAMENTO
 					channel.BasicNack(ea.DeliveryTag, false, true);
 				}
+				System.Threading.Thread.Sleep(1000);
 			};
-
-			channel.BasicConsume(queue: "pedidoRoundRobin",
+			channel.BasicConsume(queue: queueName,
 								 autoAck: false,
 								 consumer: consumer);
 
---
+++ Produtor.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Produtor.Task
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string exchangeName = "exchangeParaFilas";
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using var connection = factory.CreateConnection();
+			using var channel = connection.CreateModel();
+			channel.QueueDeclare(queue: "pedidoRoundRobin", durable: false, exclusive: false, autoDelete: false, arguments: null);
+			channel.QueueDeclare(queue: "log", durable: false, exclusive: false, autoDelete: false, arguments: null);
+			channel.QueueDeclare(queue: "pdf", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+			channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+			channel.QueueBind("pedidoRoundRobin", exchangeName, "");
+			channel.QueueBind("log", exchangeName, "");
+			channel.QueueBind("pdf", exchangeName, "");
+
+			int contador = 0;
+			while (true)
+			{
+				Console.WriteLine("");
+				Console.Write("Tecle qualquer tecla para publicar 10 mensagens");
+				Console.ReadLine();
+
+				for (int i = 0; i < 10; i++)
+				{
+					string message = $"Pedido: {contador++}";
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(exchange: exchangeName,
+										 routingKey: "",
+										 basicProperties: null,
+										 body: body);
+					Console.WriteLine(" [x] Sent {0}", message);
+				}
+			}
+		}
+	}
+}
